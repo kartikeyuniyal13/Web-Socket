c2274db8124e57ebe36b9583e9a715878df0fd03
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-06-02 09:59:18 +0530

    messages resent after reconnection

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const express = require('express');
+const { createServer } = require('http');
+const { join } = require('path');
+const { Server } = require('socket.io');
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
+
+let messages = []; // In-memory store for messages
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(join(__dirname, 'index.html'));
 });
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-// });
-
-// io.on('connection', (socket) => {
-//   socket.on('chat message', (msg) => {
-//     console.log('message: ' + msg);
-//   });
-// });
-
 
 io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle message reception and storage
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const messageId = messages.length + 1;
+    messages.push({ id: messageId, content: msg });
+    io.emit('chat message', msg, messageId);
+  });
+
+  // Handle reconnection and send missed messages
+  socket.on('reconnect', (lastMessageId) => {
+    console.log(`Reconnecting from message ID: ${lastMessageId}`);
+    const missedMessages = messages.filter(msg => msg.id > lastMessageId);
+    missedMessages.forEach(msg => {
+      socket.emit('chat message', msg.content, msg.id);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
+
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('Server running at http://localhost:3000');
 });
